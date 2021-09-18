a62edf66f6e6ccf600b4b3b4e5fdd4fb1c304a7c
Mrugank <mrugank2@gmail.com>
2021-09-18 15:42:50 -0400

    made homepage start working again

---
+++ lib/versus_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import "package:hop_swipe_cards/hop_swipe_cards.dart";
+
+class VersusPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _VersusPageState();
+}
+
+class _VersusPageState extends State<VersusPage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold();
+  }
+}
